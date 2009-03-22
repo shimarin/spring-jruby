c0c2108541fb6f7b1745a8fda91da16c1dee8799
shimarin <shimarin@fc7b3c1a-051a-0410-b5db-ddec2b85db8a>
2009-03-22 05:03:13 +0000

    git-svn-id: svn://scm.stbbs.net/spring-jruby/trunk@971 fc7b3c1a-051a-0410-b5db-ddec2b85db8a

--- src/java/net/stbbs/spring/jruby/modules/AbstractModule.java
+++
@@ -1,9 +0,0 @@
-package net.stbbs.spring.jruby.modules;
-
-import org.jruby.RubyModule;
-
-import net.stbbs.spring.jruby.SpringIntegratedJRubyRuntime;
-
-public abstract class AbstractModule {
-	public abstract void onRegister(SpringIntegratedJRubyRuntime ruby, RubyModule module);
-}
